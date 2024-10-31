e6b715fe047d13c2bc3119b67802c63f89f07e09
4jairo <jairotecles@gmail.com>
2024-10-31 17:02:14 +0100

    multiple color

--- src/db/cache.rs
+++ src/db/cache.rs
@@ -2,6 +2,7 @@ use futures::StreamExt;
 use redis::{aio::MultiplexedConnection, AsyncCommands, RedisResult};
 use tokio::sync::broadcast;
 
+use crate::routes::ws::MessageType;
 
 
 #[derive(Clone)]
@@ -9,11 +10,13 @@ pub struct Cache {
     client: MultiplexedConnection
 }
 
-pub const BITMAP_QTY: usize = 1_000_000;
+pub const BITS_PER_CHECKBOX: usize = 4;
+pub const BITS_LEN: usize = 1_000_000 * BITS_PER_CHECKBOX;
+
 
 // const REDIS_URL: &str = "redis://redis-cache:6379"; // TODO: move to .env
 const REDIS_URL: &str = "redis://localhost:6379"; // TODO: move to .env
-const BITMAP_KEY: &str = "state"; 
+const BITMAP_KEY: &str = "state";
 const BITMAP_UPDATE_CHANNEL: &str = "state_update";
 
 impl Cache {
@@ -33,21 +36,61 @@ impl Cache {
 
             RedisResult::Ok(())
         });
-    
+
         Ok(Cache { client })
     }
 
     pub async fn create_bitmap_if_null(&mut self) -> RedisResult<()> {
         let len: u32 = self.client.strlen(BITMAP_KEY).await?;
         if len == 0 {
-            self.client.setbit(BITMAP_KEY, BITMAP_QTY -1, false).await?;
+            self.client.setbit(BITMAP_KEY, BITS_LEN -1, false).await?;
         }
 
         Ok(())
     }
 
-    pub async fn set(&mut self, i: usize, value: bool, raw_msg: &String) -> RedisResult<()> {
-        self.client.setbit(BITMAP_KEY, i, value).await?;
+
+    pub async fn bitmap_modify(&mut self, i: isize, msg_type: MessageType, raw_msg: &String) -> RedisResult<()> {
+        let byte = (i * 4) / 8;
+        let first_4_bits = (i * 4) % 8 == 0;
+
+        let result: Vec<u8> = self.client.getrange(BITMAP_KEY, byte, byte + 1).await?;
+        assert!(result.len() > 0, "redis bitmap does not exist");
+
+        let bits_value = match first_4_bits {
+            true => result[0] >> 4,
+            false => result[0] & 0b00001111
+        };
+
+        let new_value = match msg_type {
+            MessageType::Add => {
+                if bits_value < 15 {
+                    bits_value + 1
+                } else {
+                    0
+                }
+            }
+            MessageType::Sub => {
+                if bits_value > 0 {
+                    bits_value - 1
+                } else {
+                    15
+                }
+            }
+            MessageType::Set(new_value) => {
+                if new_value == bits_value {
+                    return Ok(());
+                }
+                new_value
+            }
+        };
+
+        let new_byte = match first_4_bits {
+            true => (result[0] & 0b00001111) | (new_value << 4),
+            false => (result[0] & 0b11110000) | new_value
+        };
+
+        self.client.setrange(BITMAP_KEY, byte, &vec![new_byte]).await?;
         self.client.publish(BITMAP_UPDATE_CHANNEL, raw_msg).await?;
 
         Ok(())
@@ -57,3 +100,49 @@ impl Cache {
         self.client.get(BITMAP_KEY).await
     }
 }
+
+// const TEST_AMMOUNT: isize = 5_000;
+// async fn test_getrange(mut conn: MultiplexedConnection) {
+//     let now = Instant::now();
+
+//     for i in 0..TEST_AMMOUNT / 2 {
+//         {
+//             let result: String = conn.getrange(BITMAP_KEYS[0], i, i + 1).await.unwrap();
+//             let result_u8 = result.chars().nth(0).unwrap() as u8;
+            
+//             // first 4 bits
+//             let mut bits = [0; 4];
+//             for j in 0..4 {
+//                 bits[3 - j] = (result_u8 >> (j + 4)) & 1;
+//             }
+//         }
+//     }
+
+//     for i in 0..TEST_AMMOUNT / 2 {
+//         {
+//             let result: String = conn.getrange(BITMAP_KEYS[0], i, i + 1).await.unwrap();
+//             let result_u8 = result.chars().nth(0).unwrap() as u8;
+            
+//             // last 4 bits
+//             let mut bits = [0; 4];
+//             for j in 0..4 {
+//                 bits[3 - j] = (result_u8 >> j) & 1;
+//             }
+//         }
+//     }
+
+//     println!("GET RANGE {:?}", now.elapsed())
+// }
+
+// async fn test_getbit(mut conn: MultiplexedConnection) {
+//     let now = Instant::now();
+
+//     for i in 0..TEST_AMMOUNT as usize {
+//         let mut result = [false; BITS_PER_CHECKBOX];
+//         for level in 0..BITS_PER_CHECKBOX {
+//             result[level] = conn.getbit(BITMAP_KEYS[level], i).await.unwrap();
+//         }
+//     }
+
+//     println!("GET BIT {:?}", now.elapsed())
+// }
--- src/routes/ws.rs
+++ src/routes/ws.rs
@@ -3,7 +3,7 @@ use axum::extract::{State, WebSocketUpgrade, ws::{Message, WebSocket}};
 use futures::{SinkExt, StreamExt};
 use tokio::{select, sync::mpsc};
 
-use crate::db::cache::BITMAP_QTY;
+use crate::db::cache::BITS_LEN;
 use super::AppState;
 
 pub async fn ws_handler(
@@ -13,6 +13,38 @@ pub async fn ws_handler(
     upgrade.on_upgrade(|socket| async {
         handle_ws(socket, app_state).await
     })
+}
+
+pub enum MessageType {
+    Add,
+    Sub,
+    Set(u8),
+}
+impl MessageType {
+    pub fn from_str(txt: &str) -> Option<(usize, Self)> {
+        let (i, value) = txt.split_once(";")?;       
+
+        let i = i.parse::<usize>().ok()?;
+        if i >= BITS_LEN {
+            return None;
+        }
+
+        let msg_type = match value {
+            "add" => Self::Add,
+            "sub" => Self::Sub,
+            _ if value.starts_with("set;") => {
+                let (_, new_value) = value.split_once(";")?;
+                let new_value = new_value.parse::<u8>().ok()?;
+                if new_value > 15 {
+                    return None;
+                }
+                Self::Set(new_value)
+            },
+            _ => return None
+        };
+
+        Some((i, msg_type))
+    }
 }
 
 async fn handle_ws(
@@ -40,13 +72,16 @@ async fn handle_ws(
         }
     });
     
-    while let Some(Ok(Message::Text(txt))) = rx.next().await {   
-        // txt -> "2134;true"
+    while let Some(Ok(Message::Text(txt))) = rx.next().await {  
+        /* msg types:
+            1234;add
+            1234;sub
+            1234;set;15
+        */
 
-        if let Some((i, value)) = parse_msg(&txt) {
-            println!("{i} -> {value}");
-
-            if let Err(e) = app_state.cache.set(i, value, &txt).await {
+        if let Some((i, msg_type)) = MessageType::from_str(&txt) {
+            println!("{txt}");
+            if let Err(e) = app_state.cache.bitmap_modify(i as isize, msg_type, &txt).await {
                 println!("e: {}", e);
             }
         }
@@ -54,18 +89,4 @@ async fn handle_ws(
     
     // drop broadcast_rx
     let _ = stop_tx.send(());
-}
-
-
-fn parse_msg(txt: &String) -> Option<(usize, bool)> {
-    let (i, value) = txt.split_once(";")?;
-
-    let i = i.parse::<usize>().ok()?;
-    if i >= BITMAP_QTY {
-        return None;
-    }
-
-    let value = value.parse::<bool>().ok()?;
-
-    Some((i, value))
 }
