596af0d771bcbf1f5cab64790587c3869ec6c935
4jairo <jairotecles@gmail.com>
2025-02-12 11:32:58 +0100

    fix migration 2, container volume on host machine

--- migrations/0002_40_to_50_on_each_row.rs
+++ migrations/0002_40_to_50_on_each_row.rs
@@ -1,7 +1,6 @@
 use std::{env, fs::File, io::Read};
 
 use redis::{aio::MultiplexedConnection, AsyncCommands};
-use tokio::fs;
 
 const BITMAP_KEY: &str = "state";
 const BITMAP_LEN: usize = 4_000_000;
@@ -42,7 +41,7 @@ async fn main() {
     let mut new_bitmap = vec![0u8; bitmap.len()];
     update_bitmap(&bitmap, &mut new_bitmap);
 
-    let _: () = conn.setrange(BITMAP_KEY, 0, &bitmap).await.unwrap();
+    let _: () = conn.setrange(BITMAP_KEY, 0, &new_bitmap).await.unwrap();
 }
 
 
@@ -52,9 +51,9 @@ fn update_bitmap(bitmap: &Vec<u8>, new_bitmap: &mut Vec<u8>) {
     let mut current = 0;
 
     let mut temp_bitmap= vec![0u8; (bitmap.len() / (old_bytes_per_row / 2)) * (new_bytes_per_row / 2)];
-    for i in (0..bitmap.len()).step_by(old_bytes_per_row) {
-        for j in 0..new_bytes_per_row {
-            if j < old_bytes_per_row {
+    for i in (0..bitmap.len()).step_by(old_bytes_per_row / 2) {
+        for j in 0..new_bytes_per_row / 2 {
+            if j < old_bytes_per_row / 2 {
                 temp_bitmap[current] = bitmap[i + j];
             } else {
                 temp_bitmap[current] = 0;
@@ -63,6 +62,7 @@ fn update_bitmap(bitmap: &Vec<u8>, new_bitmap: &mut Vec<u8>) {
         }
     }
 
+    // removing 500_000 to 625_000 (empty values, this can change)
     let need_to_remove = temp_bitmap.len() - bitmap.len();
     new_bitmap[..500_000 / 2].copy_from_slice(&temp_bitmap[..500_000 / 2]);
     new_bitmap[500_000 / 2..].copy_from_slice(&temp_bitmap[(500_000 / 2) + need_to_remove..]);
